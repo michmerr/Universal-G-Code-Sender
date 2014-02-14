aa4ca9c16a3880db1363c750927d0785fc923915
michmerr <mike@danceswithllamas.com>
2014-02-13 16:10:55 -0800

    Fix job success determination.
    Number of commands was getting zeroed, and never getting incremented
    during the run.

---
+++ src/com/willwinder/universalgcodesender/StdIOConnection.java
@@ -0,0 +1,11 @@
+package com.willwinder.universalgcodesender;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: MerrellM
+ * Date: 1/13/14
+ * Time: 11:50 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class StdIOConnection {
+}
